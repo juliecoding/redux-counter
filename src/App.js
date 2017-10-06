f0484f4befc914ecc30c4502ecf752fc80c91126
juliecoding <julie.akohler@gmail.com>
2017-10-05 18:24:37 -0600

    And then there were some

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
-import React, { Component } from "react";
-
+import React, { Component, PropTypes } from "react";
+import {connect} from "react-redux";
 import "./App.css";
+
+import {increment} from "./ducks/counter";
+import {decrement} from "./ducks/counter";
+import {undo} from "./ducks/counter";
+import {redo} from "./ducks/counter";
 
 export class App extends Component {
 	render() {
+		const{
+			count
+			, decrement
+			, increment
+			, redo
+			, undo
+			, previousValues
+			, futureValues
+		} = this.props;
+		//const decrement = this.props.decrement;
+		console.log(this.props);
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{ count }</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={ () => increment(1) }
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={ () => increment(5) }
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={ () => decrement(1) }
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={ () => decrement(5) }
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ previousValues.length === 0 }
+							onClick={ undo }
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ futureValues.length === 0 }
+							onClick={ redo }
 						>
 							Redo
 						</button>
@@ -60,4 +76,22 @@ export class App extends Component {
 	}
 }
 
-export default App;
+App.propTypes = {    //Helps you fix bugs!!!!! 
+	count: PropTypes.number.isRequired
+	, decrement: PropTypes.func.isRequired
+	, increment: PropTypes.func.isRequired
+	, futureValues: PropTypes.arrayOf(PropTypes.number).isRequired
+	, previousValues: PropTypes.arrayOf(PropTypes.number).isRequired
+	, redo: PropTypes.func.isRequired
+	, undo: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+	return state;
+}
+const decorator = connect(mapStateToProps, {decrement, increment, undo, redo});
+const decoratedComponent = decorator(App);
+
+export default decoratedComponent;
+
+//export default connect(mapStateToProps, {increment})(App to props) ???? 
--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,60 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+const DECREMENT = "DECREMENT";
+const INCREMENT = "INCREMENT";
+const UNDO = "UNDO";
+const REDO = "REDO";
+
+const initialState = {
+    count: 0, 
+    futureValues: [], 
+    previousValues: [] 
+}
+
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT : 
+            return {
+                count: state.count + action.amount,
+                futureValues: [],
+                previousValues: [...state.previousValues, state.count]
+            };
+        case DECREMENT : 
+            return {
+                count: state.count - action.amount
+                , futureValues: []
+                , previousValues: [...state.previousValues, state.count]
+            };
+        case UNDO : 
+            return {
+                count: state.previousValues[state.previousValues.length-1]
+                , futureValues: [...state.futureValues, state.count]
+                , previousValues: state.previousValues.slice(0, state.previousValues.length-1) 
+            };
+        case REDO : 
+            return {
+                count: state.futureValues[state.futureValues.length-1]
+                , futureValues: state.futureValues.slice(0, state.futureValues.length-1)
+                , previousValues: [...state.previousValues, state.count]
+            }
+    }
+    return state;
+}
+
+export function increment( amount ) {
+    return { amount, type: INCREMENT };
+    //return {amount: amount, type: INCREMENT};
+}
+
+export function decrement (amount) {
+    return {amount, type: DECREMENT};
+}
+
+export function undo() {
+    return { type: UNDO}
+}
+
+export function redo() {
+    return {type: REDO}
+}
